de7bdc34edf28b00d7b1e341c4c95051c85498d4
Damiano <damiano1982@o2.pl>
2016-05-30 20:54:45 +0200

    plik kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,21 @@
+suma = 0
+kontrolna = -1
+peselchk = [1,3,7,9,1,3,7,9,1,3]
+try:
+    pesel = input()
+    kontrolna = int(pesel[10])
+    for i in range(10):
+        suma += peselchk[i] * int(pesel[i])
+    reszta = 10 - (suma % 10)
+    if reszta != 10:
+        if kontrolna == reszta:
+            print("1")
+        else:
+            print("0")
+    else:
+        if kontrolna == 0:
+            print("1")
+        else:
+            print("0")
+except ValueError:
+    print("0")
